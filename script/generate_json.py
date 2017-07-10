739d8a86bfcac716304a67201fe233c7a61237d0
Norihiro Shimoda <norihiro.shimoda@gmail.com>
2017-07-10 17:55:13 +0900

    Modify to insert JSON data to BigQuery through Cloud Pub/Sub

--- script/generate_json.py
+++ script/generate_json.py
@@ -2,6 +2,9 @@ import datetime
 import json
 import subprocess
 
+from google.cloud import pubsub
+
+
 dt = datetime.datetime.now()
 dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -9,7 +12,16 @@ uptime = subprocess.check_output('uptime').decode('utf-8')
 results = uptime.split(' ')
 
 feed_json = {"datetime": dt_str, 
-             "load1": float(results[11]),
-             "load15": float(results[13].strip())}
+             "load1": float(results[10]),
+             "load15": float(results[12].strip())}
 
 print(json.dumps(feed_json))
+
+TOPIC_NAME = "[PUBSUB_TOPIC_NAME]"
+pubsub_client = pubsub.Client()
+topic = pubsub_client.topic(TOPIC_NAME)
+
+data = json.dumps(feed_json).encode('utf-8')
+message_id = topic.publish(data)
+
+print("Message {} published.".format(message_id))
